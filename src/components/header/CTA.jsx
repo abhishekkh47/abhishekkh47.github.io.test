700c24adaa9ef333b2b8080fca4eed9dfca5f0c1
Abhishek Khandelwal <Abhishek.Khandelwal@lntinfotech.com>
2022-05-26 14:53:21 +0530

    testing 1-dummy code

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CV from '../../assets/Abhishek Khandelwal_10690955.pdf'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href={CV} download className='btn'>Download CV</a>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
